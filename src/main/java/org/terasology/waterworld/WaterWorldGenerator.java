571f6a689a36de6fc354d0df9c403fbbb90d2b78
Scott M Sunarto <smsunarto@yahoo.com>
2016-12-24 20:53:16 +0700

    Added Caves

--- src/main/java/org/terasology/waterworld/WaterWorldGenerator.java
+++ src/main/java/org/terasology/waterworld/WaterWorldGenerator.java
@@ -40,6 +40,10 @@ public class WaterWorldGenerator extends BaseFacetedWorldGenerator {
                 .addProvider(new SurfaceProvider())
                 .addProvider(new IslandProvider())
                 .addProvider(new SeaProvider())
+                .addProvider(new CaveFacetProvider())
+                .addProvider(new CaveToDensityProvider())
+                .addRasterizer(new CaveRasterizer())
+                .addRasterizer(new SeaRasterizer())
                 .addRasterizer(new WaterWorldRasterizer())
                 .addPlugins();
     }
